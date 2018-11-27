8e2b894fd1c825c0f82e39bc9f37f1c64c3a1528
ICDI0906 <18217060267@163.com>
2018-11-27 19:38:04 +0800

    “添加原论文”

---
+++ SeqTree.h
@@ -0,0 +1,13 @@
+//
+// Created by 张开顺 on 2018/11/27.
+//
+
+#ifndef CLOSPAN_STUDY_SEQTREE_H
+#define CLOSPAN_STUDY_SEQTREE_H
+
+#define _FIND_FREQUENT_SEQS
+#define _CALC_I_NUM_OF_ITEMS
+#define _ANOTHER_CLOSED_APPROACH
+
+
+#endif //CLOSPAN_STUDY_SEQTREE_H
